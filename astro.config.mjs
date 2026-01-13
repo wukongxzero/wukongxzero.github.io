0f9da8ab76ab7ba9d2d91fe88e310d0703123bbb
wukongxzero <wukongxzero.github.com>
2026-01-13 18:42:49 -0500

    Configure Astro for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({site: 'https://wukongxzero.github.io',});
